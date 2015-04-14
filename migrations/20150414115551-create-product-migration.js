1b49df224bcd35a3a9e26cfe7f1ca23dfdba0ce4
Milina <milinaudara@gmail.com>
2015-04-14 14:10:23 +0530

    complete server side validations

---
+++ migrations/20150414115551-create-product-migration.js
@@ -0,0 +1,41 @@
+"use strict";
+
+module.exports = {
+    up: function(migration, DataTypes, done) {
+        migration.createTable('Products', {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            createdAt: {
+                type: DataTypes.DATE
+            },
+            updatedAt: {
+                type: DataTypes.DATE
+            },
+            productName: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            costPrice: {
+                type: DataTypes.DECIMAL,
+                allowNull: false
+            },
+            sellingPrice: {
+                type: DataTypes.DECIMAL,
+                allowNull: false
+            },
+            quantity: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        }).complete(function() {
+            migration.addIndex('Products', ['productName']).complete(done);
+        });
+    },
+
+    down: function(migration, DataTypes, done) {
+     migration.dropTable('Products').complete(done);
+    }
+};
