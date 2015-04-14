7e38205f6b028515205f10af5d945a4f8154fb50
Milina <milinaudara@gmail.com>
2015-04-14 15:28:54 +0530

    change migrations id

--- migrations/20150414115551-create-product-migration.js
+++ migrations/20150414115551-create-product-migration.js
@@ -3,11 +3,11 @@
 module.exports = {
     up: function(migration, DataTypes, done) {
         migration.createTable('Products', {
-            id: {
+  /*          id: {
                 type: DataTypes.INTEGER,
                 primaryKey: true,
                 autoIncrement: true
-            },
+            },*/
             createdAt: {
                 type: DataTypes.DATE
             },
